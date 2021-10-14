c4127c185961691bda2e5540a4f384be35114123
bmpss95102 <4a7g0036@stust.edu.tw>
2021-10-14 21:50:18 +0800

    * change config.py path fix

--- docs/config.py
+++ docs/config.py
@@ -7,13 +7,14 @@ ASSETS_DIR = os.path.join(ROOT_DIR, "assets")
 
 # # Assets VHP Dir
 ASSETS_VHP_DIR = os.path.join(ASSETS_DIR, "vhp")
-ASSETS_VHP_CT_DIR = os.path.join(ASSETS_VHP_DIR, "CT Image (494 x 281)")
-ASSETS_VHP_CT_RESIZE_DIR = os.path.join(ASSETS_VHP_DIR, "CT Image Resize (1000 x 570)")
+ASSETS_VHP_CT_RESIZE_DIR = os.path.join(ASSETS_VHP_DIR, "(VKH) CT Images Resize (1000 X 570)")
+ASSETS_VHP_SEG_DIR = os.path.join(ASSETS_VHP_DIR, "(VKH) Segmented Images (1000 X 570)")
 
 # # Assets Alignment Dir
 ASSETS_ALIGNMENT_DIR = os.path.join(ASSETS_DIR, "alignment")
-ASSETS_ALIGNMENT_CT_RESIZE_DIR = os.path.join(ASSETS_ALIGNMENT_DIR, "(VKH) CT Images Resize (1000 X 570)")
-ASSETS_ALIGNMENT_SEG_DIR = os.path.join(ASSETS_ALIGNMENT_DIR, "(VKH) CT Images Resize (1000 X 570)")
+ASSETS_ALIGNMENT_CT_DIR = os.path.join(ASSETS_ALIGNMENT_DIR, "CT Image (494 x 281)")
+ASSETS_ALIGNMENT_CT_RESIZE_DIR = os.path.join(ASSETS_ALIGNMENT_DIR, "CT Image Resize (1000 x 570)")
+
 
 # Projects
 PROJECTS_DIR = os.path.join(ROOT_DIR, "projects")
