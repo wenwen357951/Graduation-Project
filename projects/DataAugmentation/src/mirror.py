5f8cf3b5f66c13e9e5c58f6a40fac54d3562db30
bmpss95102 <4a7g0036@stust.edu.tw>
2021-10-18 21:03:59 +0800

    * New DataAugmentation dir mirror.py

---
+++ projects/DataAugmentation/src/mirror.py
@@ -0,0 +1,23 @@
+import sys
+import os
+import glob
+from PIL import Image, ImageOps
+
+ROOT_DIR = os.path.abspath("../../../")
+sys.path.append(ROOT_DIR)
+
+from docs import config
+
+image_dir = config.ASSETS_ALIGNMENT_CT_RESIZE_DIR
+files = glob.glob(os.path.join(image_dir, "*.jpg"), recursive=True)
+
+
+def main():
+    for file in files:
+        im = Image.open(file)
+        im_flip = ImageOps.mirror(im)
+        im_flip.save(os.path.join(config.LOGS_DIR, "mirror/{}".format(os.path.basename(file))), quality=100)
+
+
+if __name__ == '__main__':
+    main()
