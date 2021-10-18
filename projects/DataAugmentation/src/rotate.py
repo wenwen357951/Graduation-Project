86c37b54790d18408698feceacf10bf7706890b8
bmpss95102 <4a7g0036@stust.edu.tw>
2021-10-18 22:09:20 +0800

    * Change config.py new some path and add image into assets dir

--- projects/DataAugmentation/src/rotate.py
+++ projects/DataAugmentation/src/rotate.py
@@ -8,15 +8,16 @@ sys.path.append(ROOT_DIR)
 
 from docs import config
 
-image_dir = config.ASSETS_ALIGNMENT_CT_RESIZE_DIR
+# image_dir = config.ASSETS_ALIGNMENT_CT_RESIZE_DIR
+image_dir = '/logs/MirrorCtResize (1000 x 570)'
 files = glob.glob(os.path.join(image_dir, "*.jpg"), recursive=True)
 
 
 def main():
     for file in files:
         im = Image.open(file)
-        im_flip = ImageOps.mirror(im)
-        im_flip.save(os.path.join(config.LOGS_DIR, "MirrorCtResize (1000 x 570)/{}".format(os.path.basename(file))), quality=100)
+        im_rotate =im.rotate(5)
+        im_rotate.save(os.path.join(config.LOGS_DIR, "MirrorCtTurnLeft5Degree (1000 x 570)/{}".format(os.path.basename(file))), quality=100)
 
 
 if __name__ == '__main__':
