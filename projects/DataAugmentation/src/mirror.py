7e7b4862bef100a51ba45652adb9e641ac8c2f22
bmpss95102 <4a7g0036@stust.edu.tw>
2021-10-21 01:38:57 +0800

    * change LabelGenerate main.py make all jason

--- projects/DataAugmentation/src/mirror.py
+++ projects/DataAugmentation/src/mirror.py
@@ -8,12 +8,12 @@ ROOT_DIR = os.path.abspath("../../../")
 sys.path.append(ROOT_DIR)
 from docs import config
 
-image_dir = config.ASSETS_ALIGNMENT_CT_RESIZE_DIR
-files = glob(os.path.join(image_dir, "*.jpg"), recursive=True)
+image_dir = config.ASSETS_VHP_SEG_DIR
+files = glob(os.path.join(image_dir, "*.bmp"), recursive=True)
 
 
 def main():
-    output_dir = os.path.join(config.LOGS_DIR, "MirrorCtResize (1000 x 570)")
+    output_dir = os.path.join(config.LOGS_DIR, "MirrorSegmentedImages (1000 X 570)")
     if not os.path.exists(output_dir):
         print("Output Dir isn't exists")
         os.mkdir(config.LOGS_DIR)
@@ -23,7 +23,7 @@ def main():
         print(os.path.basename(file))
         im = Image.open(file)
         im_flip = ImageOps.mirror(im)
-        im_flip.save(os.path.join(config.LOGS_DIR, "MirrorCtResize (1000 x 570)/{}".format(os.path.basename(file))),
+        im_flip.save(os.path.join(config.LOGS_DIR, "MirrorSegmentedImages (1000 X 570)/{}".format(os.path.basename(file))),
                      quality=100)
 
 
