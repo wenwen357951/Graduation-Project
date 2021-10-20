29be96773375b83eb2b84929f265dd3f8b9f9dbc
bmpss95102 <4a7g0036@stust.edu.tw>
2021-10-21 01:38:57 +0800

    * change LabelGenerate main.py make all jason

--- projects/LabelGenerate/src/main.py
+++ projects/LabelGenerate/src/main.py
@@ -1,11 +1,10 @@
 import os
 import sys
 
-
 # Import docs config
-sys.path.append("../../../")
+sys.path.append("../../../docs")
 # noinspection PyUnresolvedReferences
-from docs import config
+import config
 
 # Append modules dir to system path
 sys.path.append(config.MODULES_DIR)
@@ -14,12 +13,50 @@ sys.path.append(config.MODULES_DIR)
 import trclab.vhp as vhp
 
 if __name__ == '__main__':
-    # 讀取標籤
-    vhp_label = vhp.OrganLabel(os.path.join(config.ACP_LABEL_DIR, "peritoneal_cavity.txt"))
-    # 讀入資料集
-    seg_dataset = vhp.OrganDataset(config.ASSETS_VHP_SEG_DIR, "*.bmp")
-    # 設置標籤
-    seg_dataset.set_label(vhp_label)
-    # 匯出標記區域根據目標圖片(映射圖片)至輸出位置
-    seg_dataset.export_label_area(config.ASSETS_ALIGNMENT_CT_RESIZE_DIR, "*.jpg",
-                                  os.path.join(config.LOGS_DIR, 'acp_turn_5_degree.json'))
+    CT_DIRS = [
+        config.ASSETS_ALIGNMENT_CT_RESIZE_DIR,
+        config.ASSETS_DA_M_CT_DIR,
+        config.ASSETS_DA_CT_L_5o_DIR,
+        config.ASSETS_DA_CT_L_10o_DIR,
+        config.ASSETS_DA_CT_R_5o_DIR,
+        config.ASSETS_DA_CT_R_10o_DIR,
+        config.ASSETS_DA_M_CT_L_5o_DIR,
+        config.ASSETS_DA_M_CT_L_10o_DIR,
+        config.ASSETS_DA_M_CT_R_5o_DIR,
+        config.ASSETS_DA_M_CT_R_10o_DIR
+    ]
+    SEG_DIRS = [
+        config.ASSETS_VHP_SEG_DIR,
+        config.ASSETS_DA_M_SEG_DIR,
+        config.ASSETS_DA_SEG_L_5o_DIR,
+        config.ASSETS_DA_SEG_L_10o_DIR,
+        config.ASSETS_DA_SEG_R_5o_DIR,
+        config.ASSETS_DA_SEG_R_10o_DIR,
+        config.ASSETS_DA_M_SEG_L_5o_DIR,
+        config.ASSETS_DA_M_SEG_L_10o_DIR,
+        config.ASSETS_DA_M_SEG_R_5o_DIR,
+        config.ASSETS_DA_M_SEG_R_10o_DIR
+    ]
+    OUTPUT_NAME = [
+        "normal.json",
+        "mirror.json",
+        "normal_l5.json",
+        "normal_l10.json",
+        "normal_r5.json",
+        "normal_r10.json",
+        "mirror_l5.json",
+        "mirror_l10.json",
+        "mirror_r5.json",
+        "mirror_r10.json",
+    ]
+
+    for (ct, seg, out) in zip(CT_DIRS, SEG_DIRS, OUTPUT_NAME):
+        print(ct, seg, out)
+        # 讀取標籤
+        vhp_label = vhp.OrganLabel(os.path.join(config.ACP_LABEL_DIR, "peritoneal_cavity.txt"))
+        # 讀入資料集
+        seg_dataset = vhp.OrganDataset(seg, "*.bmp")
+        # 設置標籤
+        seg_dataset.set_label(vhp_label)
+        # 匯出標記區域根據目標圖片(映射圖片)至輸出位置
+        seg_dataset.export_label_area(ct, "*.jpg", os.path.join(config.LOGS_DIR, out))
--- projects/LabelSplash/src/graph_polygon.py
+++ projects/LabelSplash/src/graph_polygon.py
@@ -17,32 +17,32 @@ def random_color():
 
 # noinspection PyUnresolvedReferences
 def main():
-    image_dir = config.ASSETS_DATAAUGMENTATIONIMAGE_CT_TURN_LEFT_5DEGREE_DIR
+    image_dir = config.ASSETS_DA_CT_RIGHT_5o_DIR
     files = [os.path.join(image_dir, f) for f in os.listdir(image_dir)]
     img_file = random.choice(files)
 
-    with open(os.path.join(config.LOGS_DIR, "acp.json"), 'r') as json_file:
+    with open(os.path.join(config.LOGS_DIR, "CtTurnRight5Degree.json"), 'r') as json_file:
         json_data = json.load(json_file)
 
     if json_data is None:
         return
 
-    # filename_hj = "{}{}".format(os.path.basename(img_file), os.path.getsize(img_file))
-    filename_or = os.path.join(
-        config.ASSETS_DATAAUGMENTATIONIMAGE_CT_TURN_LEFT_5DEGREE_DIR,
-        "{}".format(
-            os.path.basename(img_file)
-            # os.path.basename(img_file).split('.')[0]
-        ))
-    data = json_data[filename_or]
-    # size = len(data["regions"])
+    filename_hj = "{}{}".format(os.path.basename(img_file), os.path.getsize(img_file))
+    # filename_or = os.path.join(
+    #     config.ASSETS_DA_CT_RIGHT_5o_DIR,
+    #     "{}".format(
+    #         os.path.basename(img_file)
+    #         os.path.basename(img_file).split('.')[0]
+    #     )
+    data = json_data[filename_hj]
+    size = len(data["regions"])
 
-    # image_h = cv2.imread(img_file)
+    image_h = cv2.imread(img_file)
 
-    image_o = cv2.imread(filename_or)
+    # image_o = cv2.imread(filename_or)
 
-    # mask_image_h = np.array(image_h)
-    mask_image_o = np.array(image_o)
+    mask_image_h = np.array(image_h)
+    # mask_image_o = np.array(image_o)
 
     regions = data["regions"]
     for idx in range(size):
@@ -53,11 +53,11 @@ def main():
 
         region_points = np.array([[[x, y]] for x, y in zip(points_x, points_y)])
 
-        # cv2.polylines(mask_image_h, region_points, isClosed=True, color=random_color(), thickness=3)
-        cv2.polylines(mask_image_o, region_points, isClosed=True, color=random_color(), thickness=3)
+        cv2.polylines(mask_image_h, region_points, isClosed=True, color=random_color(), thickness=3)
+        # cv2.polylines(mask_image_o, region_points, isClosed=True, color=random_color(), thickness=3)
 
-    # cv2.imshow("Image_HJ", mask_image_h)
-    cv2.imshow("Image_OR", mask_image_o)
+    cv2.imshow("Image_HJ", mask_image_h)
+    # cv2.imshow("Image_OR", mask_image_o)
 
     if cv2.waitKey(0) == ord('q'):
         cv2.destroyAllWindows()
