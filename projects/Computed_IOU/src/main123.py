1458005fe3b32ae48ab1a8ad68b53d18ced10533
4a7g0036 <4a7g0036@stust.edu.tw>
2021-12-01 20:44:58 +0800

    verify k-fold image in project/KfoldLabelGenerate/src/verify all good

--- projects/Computed_IOU/src/main123.py
+++ projects/Computed_IOU/src/main123.py
@@ -13,13 +13,7 @@ from modules.mrcnn import utils
 from modules.mrcnn.config import Config
 from modules.trclab import config as docs
 
-# import mrcnn.model as model_lib
-# import skimage.io
-# import skimage.draw
-# from mrcnn import visualize
-# from mrcnn import utils
-# from mrcnn.config import Config
-# from trclab import config as docs
+
 def inference():
     config = InferenceConfigConfig()
 
@@ -29,95 +23,60 @@ def inference():
 
     model = model_lib.MaskRCNN(mode="inference", model_dir=docs.LOGS_DIR, config=config)
     model.load_weights(MODEL_WEIGHT_PATH, by_name=True)
-
+    result = "gt_name,pred_name,IoU\n"
     for ids in dataset.image_ids:
         image, image_meta, gt_class_id, gt_bbox, gt_mask = model_lib.load_image_gt(dataset, config, ids)
+
+        # print(dataset.class_ids)
+        # print(dataset.class_names)
+        # print(gt_class_id)
+        # print("-----------")
+        # print(gt_class_id)
+        # print([CLASSES[i] for i in gt_class_id])
+
+        # visualize.display_instances(
+        #     image,
+        #     gt_bbox,
+        #     gt_mask,
+        #     gt_class_id,
+        #     class_names=CLASSES
+        # )
         info = dataset.image_info[ids]
-        print(ids)
         print("image ID: {}.{} ({}) {}".format(info["source"], info["id"], ids,
                                                dataset.image_reference(ids)))
-
         r = model.detect([image], verbose=1)[0]
-
-        print(gt_bbox.shape)
-        print(gt_class_id.shape)
-        print(gt_mask.shape)
-        print("- - - - -")
-        print(r['rois'].shape)
-        print(r['class_ids'].shape)
-        print(r['scores'].shape)
-        print(r['masks'].shape)
 
         gt_match, pred_match, overlaps = \
             utils.compute_matches(
                 gt_bbox, gt_class_id, gt_mask,
                 r['rois'], r['class_ids'], r['scores'], r['masks'])
 
-        print(gt_match)
-        print(pred_match)
-        print(overlaps)
+        for i in range(len(pred_match)):
+            gt_name = CLASSES[gt_class_id[int(pred_match[i])]]
+            pred_name = CLASSES[r['class_ids'][i]]
+            iou = (overlaps[i, int(pred_match[i])] if pred_match[i] > -1 else overlaps[i].max())
+            result += "{},{},{:.2f}\n".format(gt_name, pred_name, iou)
 
-        visualize.display_differences(
-            image,
-            gt_bbox, gt_class_id, gt_mask,
-            r['rois'], r['class_ids'], r['scores'], r['masks'],
-            CLASSES
-        )
-
-    #
-    # visualize.display_differences(image,
-    #                               gt_bbox, gt_class_id, gt_mask,
-    #                               pred_bboxes, pred_ids, pred_scores, pred_masks,
-    #                               dataset.class_names, score_threshold=0.8)
-
-    # image = imread(filename)
-    # result = model.detect([image], verbose=1)
-    # global res
-    # res = result[0]
-
-    # visualize.display_instances(
-    #     image,
-    #     r['rois'],
-    #     r['masks'],
-    #     r['class_ids'],
-    #     CLASSES,
-    #     r['scores'],
-    # )
-
-    # filename_list = glob2.glob(os.path.join(IMAGE_DIR, "*.jpg"), recursive=True)
-    # image_list = []
-    # count = 0
-    # temp_list = []
-    # for filename in filename_list:
-    #     temp_list.append(imread(filename))
-    #     if count > 2:
-    #         image_list.append(temp_list)
-    #         temp_list = []
-    #         count = 0
-    #     else:
-    #         count += 1
-    #
-    # if 0 < count <= 2 and len(temp_list) > 0:
-    #     image_list.append(temp_list)
-    #
-    # for images in image_list:
-    #     result_list = model.detect(images, verbose=1)
-    #     for result in result_list:
-
-    # result_list = model.detect(image_list, verbose=1)
-
-    # print(len(result_list))
+        # visualize.display_differences(
+        #     image,
+        #     gt_bbox, gt_class_id, gt_mask,
+        #     r['rois'], r['class_ids'], r['scores'], r['masks'],
+        #     CLASSES
+        # )
+    with open(os.path.join(docs.LOGS_DIR, "iou", "{}.csv".format(TRAINING_NAME)), "w+") as out_f:
+        out_f.write(result)
 
 
 if __name__ == '__main__':
+    PRETRAINING_WEIGHT_TYPE = "coco"
+    LOGS_WEIGHTS_DIR = "peritoneal_e_coco20211105T1435"
     MODEL_WEIGHT_PATH = os.path.join(
-        docs.LOGS_DIR,
-        "Weights/coco/peritoneal_a_coco20211104T2156",
-        "mask_rcnn_peritoneal_a_coco_0100.h5")
+        docs.LOGS_WEIGHTS_DIR , PRETRAINING_WEIGHT_TYPE,LOGS_WEIGHTS_DIR,
+        "mask_rcnn_peritoneal_e_{}_0100.h5".format(PRETRAINING_WEIGHT_TYPE))
     if not os.path.exists(MODEL_WEIGHT_PATH):
         raise FileNotFoundError("Did not find this weight " + MODEL_WEIGHT_PATH)
 
-    TRAINING_NAME = "Peritoneal_A_COCO"
+    TRAINING_NAME = "Peritoneal_E_COCO"
     IMAGE_DIR = os.path.join(docs.DATASET_KFOLD_A, "val")
     CLASSES = [line.strip() for line in
                open(os.path.join(
@@ -132,8 +91,8 @@ if __name__ == '__main__':
 
     class PeritonealDataset(utils.Dataset):
         def load_peritoneal(self, dataset_dir, subset):
-            for x in range(CLASSES_NUM):
-                self.add_class(TRAINING_NAME, x + 1, CLASSES[x])
+            for idx, x in enumerate(CLASSES):
+                self.add_class(TRAINING_NAME, idx + 1, x)
 
             assert subset in ["train", "val"]
             dataset_dir = os.path.join(dataset_dir, subset)
@@ -165,7 +124,6 @@ if __name__ == '__main__':
         def load_mask(self, image_id):
             image_info = self.image_info[image_id]
             class_names = image_info["names"]
-
             if image_info["source"] != TRAINING_NAME:
                 return super(self.__class__, self).load_mask(image_id)
 
@@ -179,7 +137,7 @@ if __name__ == '__main__':
             class_ids = np.zeros([len(info["polygons"])])
             for i, p in enumerate(class_names):
                 if p['name'] in CLASSES:
-                    class_ids[i] = CLASSES.index(p['name']) + 1
+                    class_ids[i] = CLASSES.index(p['name'])
 
             class_ids = class_ids.astype(int)
             return mask.astype(np.bool_), class_ids
@@ -187,9 +145,6 @@ if __name__ == '__main__':
         def image_reference(self, image_id):
             """Return the docs of the image."""
             info = self.image_info[image_id]
-            print("----polygons")
-            print(type(info["polygons"]))
-            print("----polygons")
             if info["source"] == TRAINING_NAME:
                 return info["id"]
             else:
