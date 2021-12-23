eba95582dfc210bc48fa3bf9a6b35c7d3e14c3cb
4a7g0036 <4a7g0036@stust.edu.tw>
2021-12-23 23:53:29 +0800

    * Fix class num error

--- projects/Computed-DetectData/src/computed_from_classes.py
+++ projects/Computed-DetectData/src/computed_from_classes.py
@@ -6,6 +6,8 @@ import sys
 import json
 import settings
 
+np.seterr(divide='ignore', invalid='ignore')
+
 #######################
 #   匯入 TRCLAB LIB  #
 ####################
@@ -26,19 +28,19 @@ class ImageDetectData:
             self.iou_list.append(float(section["iou"]))
             self.gt_classes.append(section["gt_class_id"])
             self.pr_classes.append(section["pr_class_id"])
-            self.intersections.append(section["intersection"])
-            self.gt_difference.append(section["gt_difference"])
-            self.pr_difference.append(section["pr_difference"])
+            self.intersections.append(section["intersection"])  # TP
+            self.gt_difference.append(section["gt_difference"])  # FN
+            self.pr_difference.append(section["pr_difference"])  # FP
 
-    def calc_classes_total_area(self):
-        result = dict()
-        result["gt"] = [0] * len(settings.CLASS_LIST_WITH_BG)
-        result["pr"] = [0] * len(settings.CLASS_LIST_WITH_BG)
+        self.intersections = np.array(self.intersections)
+        self.gt_difference = np.array(self.gt_difference)
+        self.pr_difference = np.array(self.pr_difference)
+
+    def get_pr_tp_area(self):
+        result = [0] * len(settings.CLASS_LIST_WITH_BG)
         for idx, (gt, pr) in enumerate(zip(self.gt_classes, self.pr_classes)):
-            class_id = gt
-            result["gt"][class_id] += self.gt_difference[idx] + self.intersections[idx]
             if gt == pr:
-                result["pr"][class_id] += self.pr_difference[idx] + self.intersections[idx]
+                result[gt] += self.pr_difference[idx] + self.intersections[idx]
 
         return result
 
@@ -47,22 +49,75 @@ def computed_from_classes(json_folder):
     json_files = glob.glob(os.path.join(json_folder, "*.json"))
 
     total_gt = np.array([0] * len(settings.CLASS_LIST_WITH_BG))
-    total_pr = total_gt.copy()
+    total_pr = np.array([0] * len(settings.CLASS_LIST_WITH_BG))
     for json_file in json_files:
         with open(json_file, "r", encoding="utf-8") as json_stream:
             json_data = json.load(json_stream)
 
-        area_list = []
         for image_name in json_data:
-            area_list.append(ImageDetectData(json_data[image_name]).calc_classes_total_area())
+            if image_name == "gt_total_area":
+                continue
 
-        for area in area_list:
-            total_gt += np.array(area["gt"])
-            total_pr += np.array(area["pr"])
+            img_detect_data = ImageDetectData(json_data[image_name])
+            total_pr += np.array(img_detect_data.get_pr_tp_area())
+
+        total_gt += np.array(json_data["gt_total_area"])
 
     result = "classes,gt_area,pr_area\n"
     for idx, name in enumerate(settings.CLASS_LIST_WITH_BG):
-        result += "{},{},{}\n".format(name, total_gt[idx], total_pr[idx])
+        result += "{},{},{}\n" \
+            .format(name, total_gt[idx], total_pr[idx])
+
+    return result
+
+
+def computed_iou(json_folder):
+    from_classes = dict()
+    for idx in range(len(settings.CLASS_LIST_WITH_BG)):
+        from_classes[str(idx)] = {}
+        from_classes[str(idx)]["intersection"] = []
+        from_classes[str(idx)]["pr_difference"] = []
+        from_classes[str(idx)]["gt_difference"] = []
+
+    for json_file in glob.glob(os.path.join(json_folder, "*.json")):
+        with open(json_file, "r", encoding="utf-8") as json_stream:
+            json_data = json.load(json_stream)
+
+        for key in json_data:
+            if key == "gt_total_area":
+                continue
+
+            for area_idx in json_data[key]:
+                pr_id = json_data[key][area_idx]["gt_class_id"]
+                gt_id = json_data[key][area_idx]["pr_class_id"]
+                if pr_id != gt_id:
+                    continue
+
+                from_classes[str(gt_id)]["intersection"].append(json_data[key][area_idx]["intersection"])
+                from_classes[str(gt_id)]["pr_difference"].append(json_data[key][area_idx]["pr_difference"])
+                from_classes[str(gt_id)]["gt_difference"].append(json_data[key][area_idx]["gt_difference"])
+
+    iou_list = [0] * len(settings.CLASS_LIST_WITH_BG)
+    dice = [0] * len(settings.CLASS_LIST_WITH_BG)
+    recall = [0] * len(settings.CLASS_LIST_WITH_BG)
+    precision = [0] * len(settings.CLASS_LIST_WITH_BG)
+    for idx in from_classes:
+        tp = np.sum(from_classes[idx]["intersection"])
+        fp = np.sum(from_classes[idx]["pr_difference"])
+        fn = np.sum(from_classes[idx]["gt_difference"])
+
+        iou_list[int(idx)] = tp / (tp + fp + fn)
+        dice[int(idx)] = (2 * tp) / ((tp + fn) + (tp + fp))
+        recall[int(idx)] = tp / (tp + fn)
+        precision[int(idx)] = tp / (tp + fp)
+
+    iou_l = np.nan_to_num(iou_list)
+    dice_l = np.nan_to_num(dice)
+    recall_l = np.nan_to_num(recall)
+    precision_l = np.nan_to_num(precision)
+    result = "class,iou,dice,recall,precision\n"
+    for idx, name in enumerate(settings.CLASS_LIST_WITH_BG):
+        result += "{},{},{},{},{}\n".format(name, iou_l[idx], dice_l[idx], recall_l[idx], precision_l[idx])
 
     return result
 
@@ -76,6 +131,9 @@ if __name__ == '__main__':
     assert os.path.exists(args.jsondir), FileNotFoundError("JSON folder not found!")
 
     output_csv = computed_from_classes(args.jsondir)
-
     with open(os.path.join(args.logs, os.path.basename(args.jsondir) + ".csv"), "w+", encoding="utf-8") as csv_file:
+        csv_file.write(output_csv)
+
+    output_csv = computed_iou(args.jsondir)
+    with open(os.path.join(args.logs, os.path.basename(args.jsondir) + "_iou.csv"), "w+", encoding="utf-8") as csv_file:
         csv_file.write(output_csv)
